fabd80672bdbeea77294a17c9ae8146b501da5a9
ChunmengYang <277048985@qq.com>
2019-08-29 14:57:19 +0800

    init

--- playmap3.py
+++ playmap3.py
@@ -2,10 +2,16 @@
 # -*- coding: UTF-8 -*-
 
 from pygame import mixer 
+import threading
+from time import sleep
 
 def playmp3():
 	mixer.init()
 	mixer.music.load('./demo.wav')
 	mixer.music.play()
 
-playmp3()
+playThread = threading.Thread(target=playmp3)
+playThread.start()
+
+while True:
+	sleep(10)
