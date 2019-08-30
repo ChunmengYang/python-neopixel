e5afd0b8033fe83e0210ef499feb688f21dd730c
ChunmengYang <277048985@qq.com>
2019-08-30 12:32:08 +0800

    init

--- rosa.py
+++ rosa.py
@@ -6,25 +6,25 @@ import numpy as np
 from time import sleep
 import threading
 from pygame import mixer 
-# import board
-# import neopixel
+import board
+import neopixel
 
-# LED_PIN = board.D18
-# LED_COUNT = 165
-# LED_BRIGHTNESS = 0.2
-# LED_ORDER = neopixel.GRB
+LED_PIN = board.D18
+LED_COUNT = 165
+LED_BRIGHTNESS = 0.2
+LED_ORDER = neopixel.GRB
 
-# class COLOR:
-# 	BLACK = (0, 0, 0)
-# 	GREEN = (0, 255, 0)
-# 	RED = (255, 0, 0)
-# 	BLUE = (0, 0, 255)
+class COLOR:
+	BLACK = (0, 0, 0)
+	GREEN = (0, 255, 0)
+	RED = (255, 0, 0)
+	BLUE = (0, 0, 255)
 
-# def getColor(max, num):
-# 	return (255*num/max, 0, 0)
+def getColor(max, num):
+	return (255*num/max, 0, 0)
 
-# pixels = neopixel.NeoPixel(LED_PIN, LED_COUNT, brightness=LED_BRIGHTNESS, auto_write=False,
-# pixel_order=LED_ORDER)
+pixels = neopixel.NeoPixel(LED_PIN, LED_COUNT, brightness=LED_BRIGHTNESS, auto_write=False,
+pixel_order=LED_ORDER)
 
 def playmp3():
 	mixer.init()
@@ -44,24 +44,24 @@ def setupBeatChroma():
 	global beat_chroma_max_index
 	y, sr = librosa.load("./demo.mp3", sr=None)
 	
-	# 八音12度的强度
-	chroma_stft = librosa.feature.chroma_stft(y=y, sr=sr)
+	# # 八音12度的强度
+	# chroma_stft = librosa.feature.chroma_stft(y=y, sr=sr)
 	# 节拍点（帧索引）
 	tempo, beats = librosa.beat.beat_track(y=y, sr=sr)
 	# 节拍点 （时间点，单位秒）
 	beat_times = librosa.frames_to_time(beats, sr=sr)
 
-	for beat in beats:
-		max_chroma = 0
-		max_chroma_index = 0
-		chr_st_index = 0
-		for chr_st in chroma_stft:
-			if chr_st[beat] > max_chroma:
-				max_chroma = chr_st[beat]
-				max_chroma_index = chr_st_index
-			chr_st_index += 1
+	# for beat in beats:
+	# 	max_chroma = 0
+	# 	max_chroma_index = 0
+	# 	chr_st_index = 0
+	# 	for chr_st in chroma_stft:
+	# 		if chr_st[beat] > max_chroma:
+	# 			max_chroma = chr_st[beat]
+	# 			max_chroma_index = chr_st_index
+	# 		chr_st_index += 1
 
-		beat_chroma_max_index.append(max_chroma_index)
+	# 	beat_chroma_max_index.append(max_chroma_index)
 
 
 setupBeatChroma()
@@ -71,9 +71,9 @@ index = 0
 for beat_time in beat_times:
 	sleep(beat_time - pre_beat_time)
 
-	print(beat_chroma_max_index[index])
-	# pixels.fill(getColor(12, beat_chroma_max_index[index]))
-	# pixels.show()
+	# print(beat_chroma_max_index[index])
+	pixels.fill(COLOR.RED)
+	pixels.show()
 
 	pre_beat_time = beat_time
 	index += 1
